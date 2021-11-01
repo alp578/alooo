a8f8213e2cb8e57e332aebf775b3446240442189
alp578 <47557618+alp578@users.noreply.github.com>
2021-11-01 23:04:42 +0300

    Add files via upload

---
+++ ClAddin.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace AddinRibbon
+{
+
+    [Plugin("AddinRibbon", "CONN", DisplayName = "AddinRibbon")]
+    [RibbonLayout("AddinRibbon.xaml")]
+    [RibbonTab("ID_CustomTab_1")]
+    [Command("ID_Button_1", ToolTip = "Show a message")]
+
+    public class ClAddin : CommandHandlerPlugin
+    {
+        public override int ExecuteCommand(string name, params string[] parameters)
+        {
+
+            switch (name)
+            {
+                case "ID_Button_1":
+                    MessageBox.Show("ID_Button_1");
+                    break;
+            }
+
+
+            return 0;
+        }
+    }
+
+}
